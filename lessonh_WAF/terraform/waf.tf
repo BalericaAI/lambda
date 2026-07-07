88d7fa0000a3fb654de1b02a35995f74666b4464
BalericaAI <balericaai@gmail.com>
2026-07-07 15:03:47 -0500

    Add WAF Web ACL configuration in waf.tf

---
+++ lessonh_WAF/terraform/waf.tf
@@ -0,0 +1,65 @@
+##############################################################
+# WAF Web ACL
+##############################################################
+
+resource "aws_wafv2_web_acl" "api_acl" {
+
+  name = "api-waf"
+
+  scope = "REGIONAL"
+
+  default_action {
+
+    allow {}
+
+  }
+
+  visibility_config {
+
+    cloudwatch_metrics_enabled = true
+
+    metric_name = "api-waf"
+
+    sampled_requests_enabled = true
+
+  }
+
+  ##########################################################
+
+  rule {
+
+    name = "AWSManagedCommonRules"
+
+    priority = 1
+
+    override_action {
+
+      none {}
+
+    }
+
+    statement {
+
+      managed_rule_group_statement {
+
+        vendor_name = "AWS"
+
+        name = "AWSManagedRulesCommonRuleSet"
+
+      }
+
+    }
+
+    visibility_config {
+
+      cloudwatch_metrics_enabled = true
+
+      metric_name = "CommonRules"
+
+      sampled_requests_enabled = true
+
+    }
+
+  }
+
+}
